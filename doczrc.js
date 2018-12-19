0c18c44d601bec11cabd268fc711087e684b3a8d
Mateus Craveiro <mccraveiro@gmail.com>
2018-12-19 12:02:32 -0200

    Load highlight.js stylesheet

--- doczrc.js
+++ doczrc.js
@@ -2,4 +2,13 @@ export default {
   src: 'src/documentation',
   theme: 'src/documentation/theme/index',
   ordering: 'ascending',
+  modifyBundlerConfig: (config) => {
+    config.resolve.extensions.push('.css')
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
+    })
+
+    return config
+  }
 }
